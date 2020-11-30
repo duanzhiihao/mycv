e00520869ff6db9d654a0ef0d0b8ec8faf89a879
duanzhiihao <duan90@purdue.edu>
2020-11-30 09:34:08 +0800

    add support for wandb

--- mycv/train.py
+++ mycv/train.py
@@ -1,6 +1,5 @@
 import os
 from pathlib import Path
-from time import time
 import argparse
 import yaml
 from tqdm import tqdm
@@ -45,21 +44,24 @@ def train():
     cfg.momentum = 0.937
     cfg.nesterov = True
     cfg.img_size = 320
-    if cfg.local_rank in [-1, 0]:
-        # initialize wandb
-        wandb.init(project='food101', group=cfg.model, name=now(),
-                   config=cfg)
-    # arguments
+    IS_MAIN = (cfg.local_rank in [-1, 0])
+    # initialize wandb
+    if IS_MAIN:
+        wbrun = wandb.init(project='food101', group=cfg.model, name=now(),
+                           config=cfg)
+        cfg = wbrun.config
+
+    # check arguments
     metric:     str = cfg.metric.lower()
     epochs:     int = cfg.epochs
     local_rank: int = cfg.local_rank
     world_size: int = int(os.environ.get('WORLD_SIZE', 1))
-    assert local_rank == int(os.environ.get('RANK', -1)), 'Currently only support single node'
-    assert cfg.batch_size % world_size == 0, 'batch_size must be multiple of CUDA device count'
-    batch_size: int = cfg.batch_size// world_size
-    if local_rank in [-1, 0]:
+    assert local_rank == int(os.environ.get('RANK', -1)), 'Only support single node'
+    assert cfg.batch_size % world_size == 0, 'batch_size must be multiple of device count'
+    batch_size: int = cfg.batch_size // world_size
+    if IS_MAIN:
         print(cfg)
-        print(hyp)
+        print('Batch size on each single GPU =', batch_size)
     # fix random seeds for reproducibility
     set_random_seeds(1)
     # device setting
@@ -83,7 +85,7 @@ def train():
     elif cfg.model == 'res101':
         from mycv.models.cls.resnet import resnet101
         model = resnet101(num_classes=101)
-        # load_partial(model, 'weights/resnet101-5d3b4d8f.pth')
+        # load_partial(model, 'weights/resnet101-5d3b4d8f.pth', verbose=IS_MAIN)
     model = model.to(device)
     # loss function
     loss_func = torch.nn.CrossEntropyLoss(reduction='mean')
@@ -93,10 +95,11 @@ def train():
         raise NotImplementedError()
         # optimizer = torch.optim.SGD(model.parameters(), lr=hyp['lr'])
     elif cfg.optimizer == 'Adam':
-        optimizer = torch.optim.Adam(model.parameters(), lr=hyp['lr'])
+        optimizer = torch.optim.Adam(model.parameters(), lr=cfg.lr)
     scaler = amp.GradScaler(enabled=cfg.amp)
 
     if cfg.resume:
+        raise NotImplementedError()
         # resume
         log_dir = Path(cfg.log_root) / cfg.resume
         assert os.path.isdir(log_dir)
@@ -106,18 +109,11 @@ def train():
         scaler.load_state_dict(checkpoint['scaler'])
         start_epoch = checkpoint['epoch'] + 1
         best_fitness = checkpoint.get(metric, 0)
-        if local_rank in [-1, 0]:
-            yaml.dump(hyp, open(log_dir / f'hyp{start_epoch}.yaml', 'w'), sort_keys=False)
-            yaml.dump(vars(cfg), open(log_dir/f'cfg{start_epoch}.yaml','w'), sort_keys=False)
     else:
         # new experiment
-        if local_rank in [-1, 0]:
-            log_dir = increment_dir(dir_root=cfg.log_root, name=cfg.model)
-            assert not os.path.exists(log_dir), log_dir
-            # make dir and save configs
-            os.makedirs(log_dir / 'weights')
-            yaml.dump(hyp, open(log_dir / 'hyp.yaml', 'w'), sort_keys=False)
-            yaml.dump(vars(cfg), open(log_dir / 'cfg.yaml', 'w'), sort_keys=False)
+        if IS_MAIN:
+            log_dir = Path(wbrun.dir) # wandb logging dir
+            assert log_dir.exists()
             print(str(model), file=open(log_dir / 'model.txt', 'w'))
         start_epoch = 0
         best_fitness = 0
@@ -132,11 +128,11 @@ def train():
         model = DDP(model, device_ids=[local_rank], output_device=local_rank)
 
     # Dataset
-    if local_rank in [-1, 0]:
+    if IS_MAIN:
         print('Initializing Datasets and Dataloaders...')
     # training set
     # trainset = ImageNetCls(split='train', img_size=hyp['img_size'], augment=True)
-    trainset = Food101(split='train', img_size=hyp['img_size'], augment=True)
+    trainset = Food101(split='train', img_size=cfg.img_size, augment=True)
     sampler = torch.utils.data.distributed.DistributedSampler(
         trainset, num_replicas=world_size, rank=local_rank, shuffle=True
     ) if local_rank != -1 else None
@@ -145,32 +141,23 @@ def train():
         num_workers=cfg.workers, pin_memory=True
     )
 
-    # Tensorboard
-    if local_rank in [-1, 0]:
-        tb_writer = SummaryWriter(log_dir)
-        print(f'Start Tensorboard with "tensorboard --logdir {log_dir}",',
-            'view at http://localhost:6006/')
-
     # ======================== start training ========================
-    val_acc = 0
-    start_time = time()
     for epoch in range(epochs):
-        train_loss, train_acc = 0.0, 0.0
-        cur_lr = optimizer.param_groups[0]['lr']
         model.train()
-
         if local_rank != -1:
             trainloader.sampler.set_epoch(epoch)
+        optimizer.zero_grad()
+
         pbar = enumerate(trainloader)
-        if local_rank in [-1, 0]:
+        if IS_MAIN:
+            train_loss, train_acc = 0.0, 0.0
+            cur_lr = optimizer.param_groups[0]['lr']
             pbar_title = ('%-10s' * 6) % (
-                'Epoch', 'GPU_mem', 'lr', 'tr_loss', 'tr_acc', 'val_acc'
+                'Epoch', 'GPU_mem', 'lr', 'tr_loss', 'tr_acc', metric
             )
             print('\n' + pbar_title) # title
             pbar = tqdm(pbar, total=len(trainloader))
-        optimizer.zero_grad()
         for i, (imgs, labels) in pbar:
-            niter = epoch * len(trainloader) + i
             # debugging
             # if True:
             #     import matplotlib.pyplot as plt
@@ -197,7 +184,8 @@ def train():
             optimizer.zero_grad()
 
             # logging
-            if local_rank in [-1, 0]:
+            if IS_MAIN:
+                niter = epoch * len(trainloader) + i
                 loss = loss.detach().cpu().item()
                 acc = cal_acc(p.detach(), labels)
                 train_loss = (train_loss*i + loss) / (i+1)
@@ -205,14 +193,16 @@ def train():
                 mem = torch.cuda.max_memory_allocated() / 1e9
                 s = ('%-10s' * 2 + '%-10.4g' * 4) % (
                     f'{epoch}/{epochs}', f'{mem:.3g}G',
-                    cur_lr, train_loss, 100*train_acc, 100*val_acc
+                    cur_lr, train_loss, 100*train_acc, 100*results[metric]
                 )
                 pbar.set_description(s)
                 torch.cuda.reset_peak_memory_stats()
                 # Tensorboard
                 if niter % 200 == 0:
-                    tb_writer.add_scalar('metric/train_loss', train_loss, global_step=niter)
-                    tb_writer.add_scalar('metric/train_acc',  train_acc,  global_step=niter)
+                    wbrun.log({
+                        'metric/train_loss': train_loss,
+                        'metric/train_acc':  train_acc
+                    }, step=niter)
                     # model.eval()
                     # results = food101_val(model, img_size=hyp['img_size'],
                     #             batch_size=4*batch_size, workers=cfg.workers)
@@ -222,21 +212,21 @@ def train():
                 # logging end
             # ----Mini batch end
         # ----Epoch end
+        # If DDP mode, synchronize model parameters on all gpus
         if local_rank != -1:
-            # Synchronize model parameters on all gpus
             model._sync_params_and_buffers(authoritative_rank=0)
 
         # Evaluation
-        if local_rank in [-1, 0]:
+        if IS_MAIN:
             model.eval()
-            results = food101_val(model, img_size=hyp['img_size'],
+            results = food101_val(model, img_size=cfg.img_size,
                         batch_size=4*batch_size, workers=cfg.workers)
-            val_acc = results['top1']
-            tb_writer.add_scalar('metric/val_acc', val_acc, global_step=niter)
-            elapsed_sec = int(time() - start_time)
-            tb_writer.add_scalar('metric/val_acc_sec', val_acc, global_step=elapsed_sec)
+            # results is like {'top1': xxx, 'top5': xxx}
+            wbrun.log(
+                {'metric/val_'+k: v for k,v in results.items()}, step=niter
+            )
             # Write evaluation results
-            res = s + '||' + '%10.4g' * 1 % (results['top1'])
+            res = s + '||' + '%10.4g' * 1 % (results[metric])
             with open(log_dir / 'results.txt', 'a') as f:
                 f.write(res + '\n')
             # save last checkpoint
@@ -247,11 +237,11 @@ def train():
                 'epoch'    : epoch,
                 metric     : results[metric]
             }
-            torch.save(checkpoint, log_dir / 'weights/last.pt')
+            torch.save(checkpoint, log_dir / 'last.pt')
             # save best checkpoint
             if results[metric] > best_fitness:
                 best_fitness = results[metric]
-                torch.save(checkpoint, log_dir / 'weights/best.pt')
+                torch.save(checkpoint, log_dir / 'best.pt')
             del checkpoint
         # ----Epoch end
     # ----Training end
