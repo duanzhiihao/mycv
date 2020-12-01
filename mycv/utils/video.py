ef53f2cac92e0996d2e94b4ee0db49f48cde8ab0
duanzhiihao <duan90@purdue.edu>
2020-12-01 23:23:04 +0800

    add a debug argument in play_video

--- mycv/utils/video.py
+++ mycv/utils/video.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import cv2
 
@@ -24,13 +25,18 @@ def load_frames(video_path, max_length=100):
     return frames, info
 
 
-def play_frames(frames: list):
-    '''
+def play_frames(frames: list, debug=False):
+    """ Play the frames as a video using cv2
+
     Args:
-        frames: list of RGB frames
-    '''
+        frames (list): list of RGB frames
+        debug (bool, optional): plt.imshow() every frame. Defaults to False.
+    """
     for i, im in enumerate(frames):
         assert imgUtils.is_image(im), f"The {i}'th frame is not a valid image"
+        if debug:
+            plt.imshow(im); plt.show()
+            continue
         im = cv2.cvtColor(im, cv2.COLOR_RGB2BGR)
         cv2.imshow('Press q to exit', im)
         if cv2.waitKey(200) & 0xFF == ord('q'):
