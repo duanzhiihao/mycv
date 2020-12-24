faa05740bf899b1ca588fa9db106d05b0b0d24e0
duanzhiihao <duan90@purdue.edu>
2020-12-24 11:56:35 +0800

    upload imagenet class names

--- mycv/unittest/check_imgnet.py
+++ mycv/unittest/check_imgnet.py
@@ -14,12 +14,12 @@ if __name__ == "__main__":
     print(len(img_paths))
     for impath in tqdm(img_paths):
         impath = str(impath) + '.JPEG'
-        assert os.path.exists(impath)
+        assert os.path.exists(impath), f'{impath} does not exist'
     # check val set
     lines = open(root / 'ImageSets/CLS-LOC/val.txt', 'r').read().strip().split('\n')
     img_paths = [root / f'Data/CLS-LOC/val/{s.split()[0]}' for s in lines]
     print(len(img_paths))
     for impath in tqdm(img_paths):
         impath = str(impath) + '.JPEG'
-        assert os.path.exists(impath)
+        assert os.path.exists(impath), f'{impath} does not exist'
     assert os.path.exists(root / 'Annotations' / 'cls_val_real.json')
