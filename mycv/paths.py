807d07ceeb1cdb66022e6c058d8501ba6430a52c
Zhihao Duan <duan90@purdue.edu>
2021-02-06 16:24:47 +0800

    switch from winddows to ubuntu; update paths

--- mycv/paths.py
+++ mycv/paths.py
@@ -12,25 +12,22 @@ MYCV_DIR = Path(os.path.dirname(__file__))
 WEIGHTS_DIR = Path(os.path.dirname(__file__)) / 'weights'
 
 # CLIC dataset: http://www.compression.cc/
-CLIC_DIR = Path('D:/Datasets/CLIC')
+CLIC_DIR = Path('/ssd1/Datasets/CLIC')
 
 # Kodak dataset: http://www.cs.albany.edu/~xypan/research/snr/Kodak.html
-KODAK_DIR = Path('D:/Datasets/Kodak')
+KODAK_DIR = Path('/ssd1/Datasets/Kodak')
 
 # ImageNet dataset
-IMAGENET_DIR = Path('D:/Datasets/imagenet')
+IMAGENET_DIR = Path('/ssd1/Datasets/imagenet')
 
 # Food-101 dataset
-FOOD101_DIR = Path('D:/Datasets/food-101')
+FOOD101_DIR = Path('/ssd1/Datasets/food-101')
 
 # COCO dataset: http://cocodataset.org/#home
-COCO_DIR = Path('D:/Datasets/COCO')
+COCO_DIR = Path('/ssd1/Datasets/COCO')
 
 # MW-R, HABBOF, and CEPDOF dataset: http://vip.bu.edu/projects/vsns/cossy/datasets/
 COSSY_DIR = Path('/home/duanzh/Projects/Datasets/COSSY')
 
-# Global Wheat Head Detection dataset
-GWHD_DIR = Path('/home/duanzh/Projects/Datasets/GWHD')
-
 # UCF-101 dataset
-UCF101_DIR = Path('D:/Datasets/UCF-101')
+UCF101_DIR = Path('/ssd1/Datasets/UCF-101')
