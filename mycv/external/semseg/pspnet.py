eaa202c26db89faff4b5d038cfa0fed7bbf4c37c
Zhihao Duan <duan90@purdue.edu>
2021-02-12 21:13:28 +0800

    simplify pspnet

--- mycv/external/semseg/pspnet.py
+++ mycv/external/semseg/pspnet.py
@@ -29,16 +29,12 @@ class PSPNet(nn.Module):
     """ PSPNet: https://arxiv.org/abs/1612.01105
     Copied from https://github.com/hszhao/semseg
     """
-    def __init__(self, layers=50, bins=(1, 2, 3, 6), dropout=0.1, classes=19, zoom_factor=8,
-                 use_ppm=True, criterion=nn.CrossEntropyLoss(ignore_index=255)):
+    def __init__(self, layers=50, bins=(1, 2, 3, 6), dropout=0.1, classes=19):
         super(PSPNet, self).__init__()
         assert layers in [50, 101, 152]
         assert 2048 % len(bins) == 0
         assert classes > 1
-        assert zoom_factor in [1, 2, 4, 8]
-        self.zoom_factor = zoom_factor
-        self.use_ppm = use_ppm
-        self.criterion = criterion
+        self.criterion = nn.CrossEntropyLoss(ignore_index=255)
 
         if layers == 50:
             resnet = resnet50()
@@ -67,9 +63,8 @@ class PSPNet(nn.Module):
                 m.stride = (1, 1)
 
         fea_dim = 2048
-        if use_ppm:
-            self.ppm = PPM(fea_dim, int(fea_dim/len(bins)), bins)
-            fea_dim *= 2
+        self.ppm = PPM(fea_dim, int(fea_dim/len(bins)), bins)
+        fea_dim *= 2
         self.cls = nn.Sequential(
             nn.Conv2d(fea_dim, 512, kernel_size=3, padding=1, bias=False),
             nn.BatchNorm2d(512),
@@ -77,36 +72,30 @@ class PSPNet(nn.Module):
             nn.Dropout2d(p=dropout),
             nn.Conv2d(512, classes, kernel_size=1)
         )
-        if self.training:
-            self.aux = nn.Sequential(
-                nn.Conv2d(1024, 256, kernel_size=3, padding=1, bias=False),
-                nn.BatchNorm2d(256),
-                nn.ReLU(inplace=True),
-                nn.Dropout2d(p=dropout),
-                nn.Conv2d(256, classes, kernel_size=1)
-            )
+        self.aux = nn.Sequential(
+            nn.Conv2d(1024, 256, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Dropout2d(p=dropout),
+            nn.Conv2d(256, classes, kernel_size=1)
+        )
 
     def forward(self, x, y=None):
-        x_size = x.size()
-        assert (x_size[2]-1) % 8 == 0 and (x_size[3]-1) % 8 == 0
-        h = int((x_size[2] - 1) / 8 * self.zoom_factor + 1)
-        w = int((x_size[3] - 1) / 8 * self.zoom_factor + 1)
+        assert (x.shape[2]-1) % 8 == 0 and (x.shape[3]-1) % 8 == 0
+        h, w = x.shape[2:4]
 
         x = self.layer0(x)
         x = self.layer1(x)
         x = self.layer2(x)
         x_tmp = self.layer3(x)
         x = self.layer4(x_tmp)
-        if self.use_ppm:
-            x = self.ppm(x)
+        x = self.ppm(x)
         x = self.cls(x)
-        if self.zoom_factor != 1:
-            x = F.interpolate(x, size=(h, w), mode='bilinear', align_corners=True)
+        x = F.interpolate(x, size=(h, w), mode='bilinear', align_corners=True)
 
         if self.training:
             aux = self.aux(x_tmp)
-            if self.zoom_factor != 1:
-                aux = F.interpolate(aux, size=(h, w), mode='bilinear', align_corners=True)
+            aux = F.interpolate(aux, size=(h, w), mode='bilinear', align_corners=True)
             main_loss = self.criterion(x, y)
             aux_loss = self.criterion(aux, y)
             return x.max(1)[1], main_loss, aux_loss
