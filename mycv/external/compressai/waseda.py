7f756480f79aa03ccbf613fecf956333e25ec545
Zhihao Duan <duan90@purdue.edu>
2021-04-30 10:45:43 +0800

    add compressai imports

--- mycv/external/compressai/waseda.py
+++ mycv/external/compressai/waseda.py
@@ -16,6 +16,15 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as tnf
 
+from compressai.layers import (
+    AttentionBlock,
+    ResidualBlock,
+    ResidualBlockUpsample,
+    ResidualBlockWithStride,
+    conv3x3,
+    subpel_conv3x3,
+)
+
 
 class Cheng2020Anchor(nn.Module):
     """Anchor model variant from `"Learned Image Compression with
@@ -52,6 +61,7 @@ class Cheng2020Anchor(nn.Module):
             subpel_conv3x3(N, 3, 2),
         )
         self.enable_bpp = enable_bpp
+        self.latent_channels = N
         if not enable_bpp:
             return
         raise NotImplementedError()
@@ -79,225 +89,6 @@ class Cheng2020Anchor(nn.Module):
         # return x_hat, (y_likelihoods, z_likelihoods)
 
 
-def conv3x3(in_ch, out_ch, stride=1):
-    """3x3 convolution with padding."""
-    return nn.Conv2d(in_ch, out_ch, kernel_size=3, stride=stride, padding=1)
-
-
-def subpel_conv3x3(in_ch, out_ch, r=1):
-    """3x3 sub-pixel convolution for up-sampling."""
-    return nn.Sequential(
-        nn.Conv2d(in_ch, out_ch * r ** 2, kernel_size=3, padding=1), nn.PixelShuffle(r)
-    )
-
-
-def conv1x1(in_ch, out_ch, stride=1):
-    """1x1 convolution."""
-    return nn.Conv2d(in_ch, out_ch, kernel_size=1, stride=stride)
-
-
-class ResidualBlockWithStride(nn.Module):
-    """Residual block with a stride on the first convolution.
-
-    Args:
-        in_ch (int): number of input channels
-        out_ch (int): number of output channels
-        stride (int): stride value (default: 2)
-    """
-
-    def __init__(self, in_ch, out_ch, stride=2):
-        super().__init__()
-        self.conv1 = conv3x3(in_ch, out_ch, stride=stride)
-        self.leaky_relu = nn.LeakyReLU(inplace=True)
-        self.conv2 = conv3x3(out_ch, out_ch)
-        self.gdn = GDN(out_ch)
-        if stride != 1 or in_ch != out_ch:
-            self.skip = conv1x1(in_ch, out_ch, stride=stride)
-        else:
-            self.skip = None
-
-    def forward(self, x):
-        identity = x
-        out = self.conv1(x)
-        out = self.leaky_relu(out)
-        out = self.conv2(out)
-        out = self.gdn(out)
-
-        if self.skip is not None:
-            identity = self.skip(x)
-
-        out += identity
-        return out
-
-
-class ResidualBlockUpsample(nn.Module):
-    """Residual block with sub-pixel upsampling on the last convolution.
-
-    Args:
-        in_ch (int): number of input channels
-        out_ch (int): number of output channels
-        upsample (int): upsampling factor (default: 2)
-    """
-
-    def __init__(self, in_ch, out_ch, upsample=2):
-        super().__init__()
-        self.subpel_conv = subpel_conv3x3(in_ch, out_ch, upsample)
-        self.leaky_relu = nn.LeakyReLU(inplace=True)
-        self.conv = conv3x3(out_ch, out_ch)
-        self.igdn = GDN(out_ch, inverse=True)
-        self.upsample = subpel_conv3x3(in_ch, out_ch, upsample)
-
-    def forward(self, x):
-        identity = x
-        out = self.subpel_conv(x)
-        out = self.leaky_relu(out)
-        out = self.conv(out)
-        out = self.igdn(out)
-        identity = self.upsample(x)
-        out += identity
-        return out
-
-
-class ResidualBlock(nn.Module):
-    """Simple residual block with two 3x3 convolutions.
-
-    Args:
-        in_ch (int): number of input channels
-        out_ch (int): number of output channels
-    """
-
-    def __init__(self, in_ch, out_ch):
-        super().__init__()
-        self.conv1 = conv3x3(in_ch, out_ch)
-        self.leaky_relu = nn.LeakyReLU(inplace=True)
-        self.conv2 = conv3x3(out_ch, out_ch)
-        if in_ch != out_ch:
-            self.skip = conv1x1(in_ch, out_ch)
-        else:
-            self.skip = None
-
-    def forward(self, x):
-        identity = x
-
-        out = self.conv1(x)
-        out = self.leaky_relu(out)
-        out = self.conv2(out)
-        out = self.leaky_relu(out)
-
-        if self.skip is not None:
-            identity = self.skip(x)
-
-        out = out + identity
-        return out
-
-
-class GDN(nn.Module):
-    r"""Generalized Divisive Normalization layer.
-
-    Introduced in `"Density Modeling of Images Using a Generalized Normalization
-    Transformation" <https://arxiv.org/abs/1511.06281>`_,
-    by Balle Johannes, Valero Laparra, and Eero P. Simoncelli, (2016).
-
-    .. math::
-
-       y[i] = \frac{x[i]}{\sqrt{\beta[i] + \sum_j(\gamma[j, i] * x[j]^2)}}
-
-    """
-
-    def __init__(self, in_channels, inverse=False, beta_min=1e-6, gamma_init=0.1):
-        super().__init__()
-
-        beta_min = float(beta_min)
-        gamma_init = float(gamma_init)
-        self.inverse = bool(inverse)
-
-        self.beta_reparam = NonNegativeParametrizer(minimum=beta_min)
-        beta = torch.ones(in_channels)
-        beta = self.beta_reparam.init(beta)
-        self.beta = nn.Parameter(beta)
-
-        self.gamma_reparam = NonNegativeParametrizer()
-        gamma = gamma_init * torch.eye(in_channels)
-        gamma = self.gamma_reparam.init(gamma)
-        self.gamma = nn.Parameter(gamma)
-
-    def forward(self, x):
-        _, C, _, _ = x.size()
-
-        beta = self.beta_reparam(self.beta)
-        gamma = self.gamma_reparam(self.gamma)
-        gamma = gamma.reshape(C, C, 1, 1)
-        norm = tnf.conv2d(x ** 2, gamma, beta)
-
-        if self.inverse:
-            norm = torch.sqrt(norm)
-        else:
-            norm = torch.rsqrt(norm)
-
-        out = x * norm
-
-        return out
-
-
-class NonNegativeParametrizer(nn.Module):
-    """
-    Non negative reparametrization.
-
-    Used for stability during training.
-    """
-
-    def __init__(self, minimum=0, reparam_offset=2 ** -18):
-        super().__init__()
-
-        self.minimum = float(minimum)
-        self.reparam_offset = float(reparam_offset)
-
-        pedestal = self.reparam_offset ** 2
-        self.register_buffer("pedestal", torch.Tensor([pedestal]))
-        bound = (self.minimum + self.reparam_offset ** 2) ** 0.5
-        self.lower_bound = LowerBound(bound)
-
-    def init(self, x):
-        return torch.sqrt(torch.max(x + self.pedestal, self.pedestal))
-
-    def forward(self, x):
-        out = self.lower_bound(x)
-        out = out ** 2 - self.pedestal
-        return out
-
-
-class LowerBoundFunction(torch.autograd.Function):
-    """Autograd function for the `LowerBound` operator."""
-
-    @staticmethod
-    def forward(ctx, input_, bound):
-        ctx.save_for_backward(input_, bound)
-        return torch.max(input_, bound)
-
-    @staticmethod
-    def backward(ctx, grad_output):
-        input_, bound = ctx.saved_tensors
-        pass_through_if = (input_ >= bound) | (grad_output < 0)
-        return pass_through_if.type(grad_output.dtype) * grad_output, None
-
-
-class LowerBound(nn.Module):
-    """Lower bound operator, computes `torch.max(x, bound)` with a custom
-    gradient.
-
-    The derivative is replaced by the identity function when `x` is moved
-    towards the `bound`, otherwise the gradient is kept to zero.
-    """
-
-    def __init__(self, bound):
-        super().__init__()
-        self.register_buffer("bound", torch.Tensor([float(bound)]))
-
-    @torch.jit.unused
-    def lower_bound(self, x):
-        return LowerBoundFunction.apply(x, self.bound)
-
-    def forward(self, x):
-        if torch.jit.is_scripting():
-            return torch.max(x, self.bound)
-        return self.lower_bound(x)
+if __name__ == '__main__':
+    model = Cheng2020Anchor(N=192, enable_bpp=False)
+    weights_path = MYCV_DIR / f'weights/compressai/cheng2020-anchor-5.pt'
