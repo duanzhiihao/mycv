801e005d8293fa05ab7cdf019dcf8a621f574ea5
Zhihao Duan <duan90@purdue.edu>
2021-05-24 13:07:08 +0800

    remove useless function

--- mycv/datasets/superres.py
+++ mycv/datasets/superres.py
@@ -9,54 +9,7 @@ import torch
 from mycv.utils.image import psnr_dB, crop_divisible
 
 
-def evaluate_folder(model, dataset, scale=2, verbose=True):
-    assert scale in (2, 4, 8)
-
-    if dataset in ['set5', 'set14']:
-        from mycv.paths import SR_DIR
-        img_dir = SR_DIR / dataset
-    else:
-        assert os.path.isdir(dataset)
-        img_dir = dataset
-
-    img_dir = Path(img_dir)
-    img_names = os.listdir(img_dir)
-    img_names.sort()
-
-    psnr_sum = 0
-    pbar = enumerate(img_names)
-    if verbose:
-        pbar = tqdm(pbar)
-    for it, imname in pbar:
-        assert imname.endswith('.png')
-        impath = img_dir / imname
-        hr = cv2.imread(str(impath))
-        hr = cv2.cvtColor(hr, cv2.COLOR_BGR2RGB)
-        hr = crop_divisible(hr, div=scale)
-        hrh, hrw = hr.shape[:2]
-
-        lr = cv2.resize(hr, (hrw//scale,hrh//scale))
-        hr_p = model.sr_numpy(lr)
-
-        _psnr = psnr_dB(hr_p, hr)
-        psnr_sum += _psnr
-
-        if verbose:
-            _imn = imname.split('.')[0]
-            _pmean = psnr_sum / (it + 1)
-            msg = f'image: {_imn}, psnr: {_psnr:.2f}, avg: {_pmean:.2f}'
-            pbar.set_description(msg)
-        if False:
-            combine = np.concatenate([hr,hr_p], axis=1)
-            plt.imshow(combine); plt.show()
-
-    results = {
-        'psnr': psnr_sum / (it + 1)
-    }
-    return results
-
-
-def evaluate_div2k(model, dataset, scale=2, verbose=True):
+def sr_evaluate(model, dataset, scale=2, verbose=True):
     assert scale in (2, 3, 4)
     from mycv.paths import DIV2K_DIR
 
