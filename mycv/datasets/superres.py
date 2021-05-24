227141ab32b0df78ec0b1921a617d4c27e7eb516
Zhihao Duan <duan90@purdue.edu>
2021-05-24 13:01:04 +0800

    add div2k dataset eval function

--- mycv/datasets/superres.py
+++ mycv/datasets/superres.py
@@ -9,8 +9,8 @@ import torch
 from mycv.utils.image import psnr_dB, crop_divisible
 
 
-def sr_evaluate(model, dataset, rate=2, verbose=True):
-    assert rate in (2, 4, 8)
+def evaluate_folder(model, dataset, scale=2, verbose=True):
+    assert scale in (2, 4, 8)
 
     if dataset in ['set5', 'set14']:
         from mycv.paths import SR_DIR
@@ -32,10 +32,10 @@ def sr_evaluate(model, dataset, rate=2, verbose=True):
         impath = img_dir / imname
         hr = cv2.imread(str(impath))
         hr = cv2.cvtColor(hr, cv2.COLOR_BGR2RGB)
-        hr = crop_divisible(hr, div=rate)
+        hr = crop_divisible(hr, div=scale)
         hrh, hrw = hr.shape[:2]
 
-        lr = cv2.resize(hr, (hrw//rate,hrh//rate))
+        lr = cv2.resize(hr, (hrw//scale,hrh//scale))
         hr_p = model.sr_numpy(lr)
 
         _psnr = psnr_dB(hr_p, hr)
@@ -56,6 +56,70 @@ def sr_evaluate(model, dataset, rate=2, verbose=True):
     return results
 
 
+def evaluate_div2k(model, dataset, scale=2, verbose=True):
+    assert scale in (2, 3, 4)
+    from mycv.paths import DIV2K_DIR
+
+    if dataset == 'div2k_bicubic':
+        hr_dir = DIV2K_DIR / 'val_hr'
+        lr_dir = DIV2K_DIR / f'val_lr_bicubic/x{scale}'
+    elif dataset in ['set5', 'set14']:
+        from mycv.paths import SR_DIR
+        hr_dir = SR_DIR / dataset
+        lr_dir = None
+    else:
+        assert os.path.isdir(dataset)
+        hr_dir = Path(dataset)
+        lr_dir = None
+
+    hr_names = os.listdir(hr_dir)
+    hr_names.sort()
+
+    psnr_sum = 0
+    pbar = enumerate(hr_names)
+    if verbose:
+        pbar = tqdm(pbar)
+    for it, hrname in pbar:
+        # get high-resolution image
+        assert hrname.endswith('.png')
+        hrpath = hr_dir / hrname
+        hr = cv2.imread(str(hrpath))
+        hr = cv2.cvtColor(hr, cv2.COLOR_BGR2RGB)
+        hr = crop_divisible(hr, div=scale)
+        hrh, hrw = hr.shape[:2]
+        assert hrh % scale == 0 and hrw % scale == 0
+
+        # get low-resolution image
+        if lr_dir is not None:
+            lrpath = lr_dir / hrname.replace('.png', f'x{scale}.png')
+            lr = cv2.imread(str(lrpath))
+            lr = cv2.cvtColor(lr, cv2.COLOR_BGR2RGB)
+        else:
+            lr = cv2.resize(hr, (hrw//scale,hrh//scale))
+        assert lr.shape[:2] == (hrh//scale, hrw//scale)
+
+        # predict high-res image
+        with torch.no_grad():
+            hr_p = model.sr_numpy(lr)
+
+        _psnr = psnr_dB(hr_p, hr)
+        psnr_sum += _psnr
+
+        if verbose:
+            _imn = hrname.split('.')[0]
+            _pmean = psnr_sum / (it + 1)
+            msg = f'image: {_imn}, psnr: {_psnr:.2f}, avg: {_pmean:.2f}'
+            pbar.set_description(msg)
+        # if True:
+        #     combine = np.concatenate([hr,hr_p], axis=1)
+        #     plt.imshow(combine); plt.show()
+
+    results = {
+        'psnr': psnr_sum / (it + 1)
+    }
+    return results
+
+
 if __name__ == '__main__':
     class test():
         def sr_numpy(self, lr):
@@ -64,5 +128,5 @@ if __name__ == '__main__':
             return hr
     
     model = test()
-    results = sr_evaluate(model, 'set14', rate=2)
+    results = sr_evaluate(model, 'set14', scale=2)
     print(results)
