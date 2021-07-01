036189ff7f7b2fff22d465e93ad056d09eabba70
Zhihao Duan <duan90@purdue.edu>
2021-07-02 00:37:56 +0800

    remoce deprecated lr schedular code

--- mycv/utils/torch_utils.py
+++ mycv/utils/torch_utils.py
@@ -24,6 +24,21 @@ def num_params(model: nn.Module):
     return num
 
 
+def model_profile(model: nn.Module, input):
+    """ get the model params and FLOPs
+
+    Args:
+        model (nn.Module): pytorch model
+    """
+    nparam = num_params(model)
+    
+    from fvcore.nn import flop_count
+    flops_count, skipped_ops = flop_count(model, (input, )) 
+    print(flops_count)
+
+    return nparam, flops_count
+
+
 def summary_weights(state_dict: OrderedDict, save_path='model.txt'):
     if not isinstance(state_dict, OrderedDict):
         print('Warning: state_dict is not a OrderedDict. keys may not be ordered.')
@@ -156,39 +171,6 @@ def warmup_cosine(n, min_lrf, warmup_iter, total_iter):
         _cur = n - warmup_iter + 1
         factor = min_lrf + 0.5 * (1 - min_lrf) * (1 + math.cos(_cur * math.pi / total_iter))
     return factor
-
-
-def adjust_lr_threestep(optimizer, cur_epoch, base_lr, total_epoch):
-    """ Sets the learning rate to the initial LR decayed by 10 every total/3 epochs
-
-    Args:
-        optimizer (torch.optim.Optimizer): optimizer
-        cur_epoch (int): current epoch
-        base_lr (float): base learning rate
-        total_epoch (int): total epoch
-    """
-    from mycv.utils.general import warning
-    warning('torch_utils.adjust_lr_threestep() is deprecated. Use lr_schedulers.')
-    assert total_epoch >= 3
-    period = math.ceil(total_epoch / 3)
-    lr = base_lr * (0.1 ** (cur_epoch // period))
-    for param_group in optimizer.param_groups:
-        param_group['lr'] = lr
-
-def _adjust_lr_532(optimizer, cur_epoch, base_lr, total_epoch):
-    from mycv.utils.general import warning
-    warning('torch_utils._adjust_lr_532() is deprecated. Use lr_schedulers.')
-    assert total_epoch % 10 == 0
-    if cur_epoch < round(total_epoch * 5/10):
-        lrf = 1
-    elif cur_epoch < round(total_epoch * 8/10):
-        lrf = 0.1
-    else:
-        assert cur_epoch < total_epoch
-        lrf = 0.01
-    lr = base_lr * lrf
-    for param_group in optimizer.param_groups:
-        param_group['lr'] = lr
 
 
 def fuse_conv_and_bn(conv, bn):
