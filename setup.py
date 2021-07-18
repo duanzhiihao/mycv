c84ac93ed87cea862d1c2b8e260d46645ec9d120
Zhihao Duan <duan90@purdue.edu>
2021-07-18 23:09:21 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+if __name__ == '__main__':
+    packages = find_packages(
+        exclude=('*deprecated*', '*scripts*', '*unittest*'),
+        include=('mycv')
+    )
+
+    setup(
+        name='mycv',
+        version='0.1',
+        description='A personal computer vision repository using pytorch',
+        author='Zhihao Duan',
+        author_email='duan90@purdue.edu',
+        url='https://github.com/duanzhiihao/mycv',
+        packages=packages
+    )
